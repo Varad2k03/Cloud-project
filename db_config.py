6c9edc1050a8f64c966440c356010e00be51555b
Varad Chandrawar <115485413+Varad2k03@users.noreply.github.com>
2026-04-13 13:48:01 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=st.secrets["MYSQLHOST"],
+        user=st.secrets["MYSQLUSER"],
+        password=st.secrets["MYSQLPASSWORD"],
+        database=st.secrets["MYSQLDATABASE"],
+        port=int(st.secrets["MYSQLPORT"])
     )
